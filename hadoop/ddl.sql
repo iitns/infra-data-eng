9f3aaced1aac10b77183ca1d35b0272dc88b50de
JangHan Park <iitns314@gmail.com>
2024-06-18 16:58:59 -0700

    initial commit

--- hadoop/ddl.sql
+++ hadoop/ddl.sql
@@ -1,4 +1,19 @@
+CREATE EXTERNAL TABLE auction(
+auction_id int,
+item_id int,
+bid int,
+buyout int,
+quantity int,
+ts timestamp
+) PARTITIONED BY (
+year int, month int, day int, hour int, region string, realm_id int, auction_house_id int
+) STORED AS PARQUET
+LOCATION 's3a://bucket-name/prefix/';
+
+MSCK REPAIR TABLE auction;
+
+/*
 create external table customer_text(id string, fname string, lname string) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' STORED AS TEXTFILE location 's3a://customer-data-text/';
 
-
 create external table customer_json(customer map<string, string>) row format serde 'org.openx.data.jsonserde.JsonSerDe' location 's3a://customer-data-json/';
+*/
--- presto/ddl.sql
+++ presto/ddl.sql
@@ -1,1 +1,14 @@
+CREATE TABLE IF NOT EXISTS
+auction(
+  auction_id BIGINT,
+  item_id BIGINT,
+  bid BIGINT,
+  buyout BIGINT,
+  quantity SMALLINT,
+  ts TIMESTAMP
+) WITH (
+  external_location = 's3a://bucket-name/prefix/*',
+  format = 'PARQUET'
+)
+
 create table customer_orc(id varchar,fname varchar,lname varchar) with (format = 'ORC', external_location = 's3a://customer-data-orc/');
